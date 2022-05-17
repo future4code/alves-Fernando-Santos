527da681770477cea9958d0d66d6e660cdf509fe
Fernandosantanasto <fernandosantana.sto@gmail.com>
2022-05-16 22:04:55 -0300

    objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,20 @@
+
+
+//1- Exercícios de interpretação de código
+//a) no console filme.elenco pelo valor 0 atribuído vai ser impresso Matheus Nachtergaele. No segundo console valor -1,
+//vai ser impresso Virginia Cavendish e no terceiro console valor atribuído 2 vai ser impresso Globo 14h
+
+//2-a) Vai impresso tudo que está na constante cachorro "Juca, 3 e SRD" e como nos outros consoles é utilizado o spread,
+// são impresso as mesmas informações do primeiro console.
+//b) A sintaxe dos três pontos serve para fazer uma cópia do objeto ou ter acesso só algumas das propriedades.
+
+//3-a) Vai ser impresso "FALSE" e "UNDEFINED" 
+//b) o valor que foi chamado no console "backender" foi atribuído "false", e no segundo console não foi atribuído
+// o valor d "altura".
+
+/*const pessoa = {
+    nome: "Amanda",
+    apelidos: ["Amandinha", "Mandinha", "Mandi"]
+}
+
+console.log(`Oi meu nome é ${pessoa.nome}, mas pode me chamar de ${pessoa.apelidos}.`)
